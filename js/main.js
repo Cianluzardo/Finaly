5c7836c02b8a6c6e17bba4e92ce68afee295ac02
Cecilia <cecilia_luzardo@hotmail.com>
2023-10-27 22:10:36 -0300

    modificaionesprimeras

---
+++ js/main.js
@@ -0,0 +1,19 @@
+var edad = 0;
+
+while (edad < 18) {
+    edad = prompt("Por favor, introduce tu edad:");
+
+    if (edad === null || isNaN(edad) || edad === "") {
+        alert("Debes introducir una edad válida para continuar.");
+        continue;
+    }
+    
+    edad = parseInt(edad); 
+
+    if (edad < 18) {
+        alert("Lo siento, debes ser mayor de 18 años para navegar.");
+    }
+}
+
+alert("¡Bienvenido! Ahora puedes navegar en el sitio.");
+
