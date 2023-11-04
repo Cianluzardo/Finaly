235149a315d251f7344656a8eccc41971a45414d
Cecilia <cecilia_luzardo@hotmail.com>
2023-11-03 21:11:07 -0300

    variables funciones metodos y eventos

--- js/main.js
+++ js/main.js
@@ -1,8 +1,22 @@
+/*alerta! 
+variables --  CONDICIONALES ---metodos */
 let edad = parseInt(prompt("Por favor, introduce tu edad:"));
 
-    if (edad <18) {
-        alert("Oh no, debes ser mayor de 18 años para navegar.");
-    } else if (edad === null || isNaN (edad) || edad ===""){
-        alert ("Debes introducir tu edad válida para continuar.");
-    }else { alert ("¡Bienvenido! Ahora puedes navegar en el sitio.")
+if (edad < 18) {
+    alert("Oh no, debes ser mayor de 18 años para navegar.");
+} else if (edad === null || isNaN(edad) || edad === "") {
+    alert("Debes introducir tu edad válida para continuar.");
+} else {
+    alert("¡Bienvenido! Ahora puedes navegar en el sitio.")
 }
+
+/* funciones-- evento--*/
+function mostrarFrase (){
+    var elemento = document.getElementById ("Cian Lú")
+    elemento.innerText= "Comprando hoy, desbloquea tu descuento";
+
+}
+function reiniciarTexto() {
+    var elemento = document.getElementById("Cian Lú");
+    elemento.innerText = "Cian Lú";
+  }
